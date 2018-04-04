566c5dad52d1eae69b225529df04b1cd1d92219c
karishma singh <karishma1294#gmail.com>
2018-04-04 21:44:29 +0530

    Add test GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request=require('supertest');
 const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
 
+const todos=[{
+  text:"First todo"
+},{
+   text:"Second todo"
+}];
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+    Todo.insertMany(todos);
+  }).then(()=>done());
 
 });
 
@@ -25,12 +33,45 @@ describe('POST /todos',()=>{
       if(err){
         return done(err);//stops the function
       }
-      Todo.find().then((todos)=>{
+      Todo.find({text}).then((todos)=>{
         expect(todos.length).toBe(1);
-        expect(todos[0].lenth).toBe(text);
+        expect(todos[0].text).toBe(text);
         done();
       }).catch((e)=>done(e));
     });
   });
 
+it('should not create new todo with invalid text',(done)=>{
+
+    request(app)
+    .post('/todos')
+    .send({})
+    .expect(400)
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+    });
+
+    Todo.find().then((todos)=>{
+      expect(todos.length).toBe(2);
+      done();
+    }).catch((e)=>done(e));
+  });
+
+
 });
+
+describe('GET /todos',()=>{
+  it("should get all todos",(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  });
+
+
+})
