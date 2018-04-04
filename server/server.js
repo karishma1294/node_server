d8e9e06a58273b7267416bbedbe536e95ed2f60d
karishma singh <karishma1294#gmail.com>
2018-04-05 01:19:17 +0530

    Added GET /todos/id routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser=require("body-parser");
+var {ObjectID}=require('mongodb');
 
 var {mongoose} =require('./db/mongoose');
 var {Todo}=require("./models/todo");
@@ -41,6 +42,22 @@ app.post('/todos',(req,res)=>{
     res.status(400).send(error);
   });
 
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+ if(!ObjectID.isValid(id)){
+   return res.status(404).send({});
+ }
+   Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+     }
+     console.log("Id found",todo);
+     res.send({todo});
+   }).catch((e,res)=>{
+     res.status(400).send({});
+   });
 });
 
 app.listen(3000,()=>{
