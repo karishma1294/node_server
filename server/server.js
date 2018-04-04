93f7f70796e1c97c088e31358590e70c901d98e9
karishma singh <karishma1294#gmail.com>
2018-04-04 21:16:11 +0530

    Add GET /todos routes

--- server/server.js
+++ server/server.js
@@ -9,31 +9,42 @@ var app=express();
 
 app.use(bodyParser.json());
 
-app.post('/Users',(req,res)=>{
-  var newUser=new User({
-    email:req.body.text
-  });
-  newUser.save().then((doc)=>{
-    res.send(doc);
-  },(err)=>{
-    res.status(404).send(err);
-  });
+// app.post('/Users',(req,res)=>{
+//   var newUser=new User({
+//     email:req.body.text
+//   });
+//   newUser.save().then((doc)=>{
+//     res.send(doc);
+//   },(err)=>{
+//     res.status(404).send(err);
+//   });
+// });
+
+
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+      res.send({todos});
+    },(e)=>{
+      res.status(400).send(e);
+    });
 });
 
 
-// app.post('/todos',(req,res)=>{
-//   var newTodo=new Todo({
-//     text:req.body.text
-//   });
-//
-//   newTodo.save().then((doc)=>{
-//     res.send(doc);
-//   },(error)=>{
-//     res.status(400).send(error);
-//   });
-//
-// });
-//
+app.post('/todos',(req,res)=>{
+  var newTodo=new Todo({
+    text:req.body.text
+  });
+
+  newTodo.save().then((doc)=>{
+    res.send(doc);
+  },(error)=>{
+    res.status(400).send(error);
+  });
+
+});
+
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
+
+module.exports={app};
