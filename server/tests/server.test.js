f646c0375dcba4b887bdb26a8d5475e5a624a454
karishma singh <karishma1294#gmail.com>
2018-05-20 00:16:38 +0530

    Add test for post /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -246,6 +246,54 @@ it('it should not create user if email in use',(done)=>{
   .expect(400)
   .end(done);
 })
+})
 
 
+describe("POST /users/login",()=>{
+
+  it('should allow user to login',(done)=>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+    password:users[1].password})
+    .expect(200)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toExist();
+    })
+    .end((err,res)=>{
+      if(err){
+          return done(err);
+      }
+      User.findById(users[1]._id).then((user)=>{
+        expect(user.tokens[0]).toInclude({
+          access:'auth',
+          token:res.headers['x-auth']
+        });
+        done();
+      }).catch((err)=>done(e));
+    })
+  })
+
+  it('should not allow login ',(done)=>{
+
+    request(app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+      password:"123"
+    })
+    .expect(401)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toNotExist();
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+      done();
+    })
+
+
+  })
 })
